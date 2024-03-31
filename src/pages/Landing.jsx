45e1b011870231f8fe8c4d6bfda5ba98141b3fb8
Vanshaj Bhatnagar <vanshajbhatnagar10@gmail.com>
2024-03-31 16:12:02 +0530

    added view resume button on landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -22,16 +22,16 @@ export function Landing() {
         <button
           className="relative block p-[2px] mx-auto mt-8 lg:mt-16 font-semibold leading-6 text-white no-underline rounded-full shadow-2xl cursor-pointer bg-slate-800 group shadow-zinc-900 text-sm lg:text-xl lg:my-8"
           onClick={() => {
-            document
-              .getElementById("contact")
-              .scrollIntoView({ behavior: "smooth" });
+            window.open(
+              "https://drive.google.com/file/d/1sCgzj-_nevu6Z3MpKNf5c9dfb8L21Bar"
+            );
           }}
         >
           <span className="absolute inset-0 overflow-hidden rounded-full">
             <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(100%_100%_at_50%_0%,rgba(24,204,253,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
           </span>
           <div className="relative z-10 flex items-center px-8 py-2 space-x-2 rounded-full lg:py-3 lg:px-10 bg-zinc-950 ring-1 ring-white/10 ">
-            <span>Let's Connect &#8680;</span>
+            <span>View Resume &#8680;</span>
           </div>
           <span className="absolute -bottom-0 left-[1.125rem] h-4 w-[calc(100%-2.25rem)] bg-gradient-to-r from-[#6344F5]/0 via-[#6344F5]/90 to-[#6344F5]/0 transition-opacity duration-500 group-hover:opacity-50"></span>
         </button>
